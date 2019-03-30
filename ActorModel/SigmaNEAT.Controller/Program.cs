b538d7de97ced51daf41c84bfcc52243bbe9ee07
Emad Hosseini <EmadHosseini@hotmail.com>
2019-03-30 12:18:54 +0430

    Orleans in place

--- ActorModel/SigmaNEAT.Controller/Program.cs
+++ ActorModel/SigmaNEAT.Controller/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using SigmaNEAT.ActorCore;
+using System;
+using System.Threading.Tasks;
 
 namespace SigmaNEAT.Controller
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to start controller");
+            Console.ReadKey();
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            try
+            {
+                using (var client = await ConnectClient())
+                {
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\nException while trying to run client: {e.Message}");
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        private static async Task<IClusterClient> ConnectClient()
+        {
+            IClusterClient client;
+            client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "Server1";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Client successfully connected to silo host \n");
+            return client;
+        }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+            var network = client.GetGrain<INetworkGrain>(0);
+            var response = await network.GetOutput(new[] { 1.0 });
+            Console.WriteLine("\n\n{0}\n\n", response);
         }
     }
 }
--- ActorModel/SigmaNEAT.Silo/Program.cs
+++ ActorModel/SigmaNEAT.Silo/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using SigmaNEAT.Actors;
+using System;
+using System.Threading.Tasks;
 
 namespace SigmaNEAT.Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("\nSilo running.\n\nPress Enter to terminate...\n\n");
+                Console.ReadLine();
+
+                await host.StopAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            // define the cluster configuration
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "Server1";
+                })
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(BasicNetworkGrain).Assembly).WithReferences())
+                .ConfigureLogging(logging => logging.AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
         }
     }
 }
