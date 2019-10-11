bf5f521a089204adcb8096a922a4e978cbd33523
Emad Hosseini <EmadHosseini@hotmail.com>
2019-10-11 15:32:42 +0330

    Progress in paper. This is where the paper data got extracted from.

--- SigmaNeatPlus/config.hpp
+++ SigmaNeatPlus/config.hpp
@@ -3,7 +3,7 @@
 #define CONFIGURED
 
 // System
-constexpr auto SYSTEM__USE_GPU = true;
+constexpr auto SYSTEM__USE_GPU = false;
 constexpr auto SYSTEM__MAX_GENERATION_COUNT = 1000;
 constexpr auto SYSTEM__THREADS_PER_BLOCK = 512;
 
@@ -15,7 +15,7 @@ constexpr auto SUBSTRATE__LAYERS_COUNT = 5;
 constexpr auto SUBSTRATE__LAYER_SIZE = 5;
 
 // Params
-constexpr auto PARAMS__POPULATION_SIZE = 100;
+constexpr auto PARAMS__POPULATION_SIZE = 10;
 constexpr auto PARAMS__EVICTION_SIZE = 50;
 constexpr auto PARAMS__WEIGHT_THRESHOLD = 0.05;
 constexpr auto PARAMS__TRAINING_GENERATIONS = 1;
